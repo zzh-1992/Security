9753fbc42cddafa6373eb7532c64cedeab512ce2
ZhangZhihuang <ezzh1992@icloud.com>
2020-09-21 22:55:21 +0800

    添加Git的相关命令

--- webtoken/src/main/java/com/grapefruit/webtoken/config/WebConfig.java
+++ webtoken/src/main/java/com/grapefruit/webtoken/config/WebConfig.java
@@ -26,6 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
+    //官网链接:https://docs.spring.io/spring/docs/current/spring-framework-reference/web.html#mvc-config-interceptors
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LocaleChangeInterceptor());
